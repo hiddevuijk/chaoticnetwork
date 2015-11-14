0a277fdda45ac26633d185e01c333dccc7d03300
hiddevuijk <hdvuijk@gmail.com>
2015-11-14 16:30:00 +0100

    add nr headers

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace::std;
+
+
+
+int main()
+{
+
+	int Ne = 1000;
+	int Ni = 1000;
+	int No = 1000;
+	
+	double Jee = 1.0;
+	double Jie = 1.0;
+	double Jeo = 1.0;
+
+
+
+
+	return 0
+}
+
+
+
+
+
+
+
